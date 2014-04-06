83cf224075f2018b5bbdfad6dda3fd3fa4b73649
Tim Huppertz <mitroman@naturalnet.de>
2014-04-06 21:52:56 +0200

    Added killer_antivurs.png changed init.lua .

--- init.lua
+++ init.lua
@@ -5,20 +5,52 @@ minetest.register_node("killer:killer", {
 		light_source = 14,
 })
 
+minetest.register_node("killer:antikiller", {
+	tiles = {"killer_antikiller.png"},
+	groups = {crumbly=1},
+	description = "Antikiller",
+})
+
 minetest.register_abm({
-		nodenames = {"killer:killer"},
-		chance = 5,
-		interval = 5,
-		action = function(pos)
-			local p = {
-				x=pos.x+(math.random(1, 3) * math.random(-1, 1)),
-				y=pos.y+(math.random(1, 3) * math.random(-1, 1)),
-				z=pos.z+(math.random(1, 3) * math.random(-1, 1))
+	nodenames = {"killer:antikiller"},
+	chance = 1,
+	interval = 1,
+	action = function(pos)
+		local p = {
+			pos1 = {x=pos.x, y=pos.y+1, z=pos.z},
+			pos2 = {x=pos.x, y=pos.y-1, z=pos.z},
+			pos3 = {x=pos.x+1, y=pos.y, z=pos.z},
+			pos4 = {x=pos.x-1, y=pos.y, z=pos.z},
+			pos5 = {x=pos.x, y=pos.y, z=pos.z+1},
+			pos6 = {x=pos.x, y=pos.y, z=pos.z-1},
 			}
-			if minetest.get_node(p).name ~= "killer:killer" then
-				minetest.set_node(p, {name="killer:killer"})
+		local is = false
+		for k, v in pairs(p) do
+			if minetest.get_node(v).name == "killer:killer" then
+				minetest.set_node(v, {name="killer:antikiller"})
+				is = true
 			end
-		end,
+		end
+		if not is then
+			minetest.remove_node(pos)
+		end
+	end,
+})
+
+minetest.register_abm({
+	nodenames = {"killer:killer"},
+	chance = 5,
+	interval = 5,
+	action = function(pos)
+		local p = {
+			x=pos.x+(math.random(1, 2)*math.random(-1, 1)),
+			y=pos.y+(math.random(1, 2)*math.random(-1, 1)),
+			z=pos.z+(math.random(1, 2)*math.random(-1, 1)),
+		}
+		if minetest.get_node(p).name ~= "killer:killer" then
+			minetest.set_node(p, {name="killer:killer"})
+		end
+	end,
 })
 
 minetest.register_craft({
@@ -28,4 +60,13 @@ minetest.register_craft({
 		{"default:steel_ingot", "", "default:steel_ingot"},
 		{"default:gold_ingot", "default:steel_ingot", "default:gold_ingot"},
 	},
+})
+
+minetest.register_craft({
+	output="killer:antikiller 1",
+	recipe = {
+		{"", "default:dirt", ""},
+		{"default:dirt", "", "default:dirt"},
+		{"", "default:dirt", ""},
+		},
 })
